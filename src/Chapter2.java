97145956a79b46ee2ae33606f7c97bb125f3722f
MississippiBrenn <cararbrennan@gmail.com>
2023-06-15 17:45:00 -0700

    nodes

---
+++ src/Chapter2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Chapter2 {
+
+    //delete a node from a linked list
+    public static void deleteNode(LinkedList<Integer>  list){
+
+        System.out.println("string of linked list ")
+    }
+}
+
